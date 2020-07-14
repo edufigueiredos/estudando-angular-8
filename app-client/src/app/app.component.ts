e9276bec4e2ab2ea7d08968103e63071484fb7dc
edufigueiredos <eduardo_figueiredo@id.uff.br>
2020-07-14 19:27:01 -0300

    Apagando projeto que não vou usar

--- app-client/src/app/app.component.ts
+++ app-client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './products.service';
+import { Observable } from 'rxjs';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-client';
+export class AppComponent implements OnInit {
+
+    simpleReqProduct$: Observable<Product[]>;
+
+    constructor(private productsService: ProductsService) { }
+
+    ngOnInit() {
+    }
+
+    getSimpleHttpRequest() {
+        this.simpleReqProduct$ = this.productsService.getProducts();
+    }
 }
--- app-client/src/app/app.module.ts
+++ app-client/src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatIconModule,
+        MatButtonModule,
+        MatListModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
